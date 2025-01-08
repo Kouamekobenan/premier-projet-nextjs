7f378f3e32d48a581825841d311c86b42a8d71eb
nelson12 <kouamenelson47@gmail.com>
2025-01-08 10:34:48 +0100

    apprendre la reation des routes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,47 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>je suis footer</h1>
+    <div className='p-4'>
+      <div className="flex gap-4 justify-around">
+        <section className='flex flex-col'>
+          <span>Ressource</span>
+          <span className='flex flex-col gap-3  text-center'>
+            <small>doc</small>
+            <small>apprendre</small>
+            <small>virtine</small>
+            <small>blog</small>
+          </span>
+        </section>
+        <section className='flex flex-col'>
+          <span>Ressource</span>
+          <span  className='flex flex-col gap-3  text-center'>
+            <small>doc</small>
+            <small>apprendre</small>
+            <small>virtine</small>
+            <small>blog</small>
+          </span>
+        </section>
+        <section className='flex flex-col'>
+          <span>Ressource</span>
+          <span  className='flex flex-col gap-3  text-center'>
+            <small>doc</small>
+            <small>apprendre</small>
+            <small>blog</small>
+          </span>
+        </section>
+        <section className='flex flex-col'>
+          <span>Ressource</span>
+          <span  className='flex flex-col gap-3 text-center'>
+            <small>doc</small>
+            <small>apprendre</small>
+            <small>virtine</small>
+            <small>blog</small>
+          </span>
+        </section>
+        
+        
+      </div>
+      
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
@@ -8,8 +9,8 @@ const Header = () => {
           <span className='text-2xl font-thin'>Nextjs </span>
         </section>
         <section className='flex md:flex-row md:justify-between gap-6'>
-            <span>Documentation</span>
-            <span>cours</span>
+           <Link href="/documentation">Documentation</Link> 
+           <Link href="/cours"> cours </Link>
             <span>connexion</span>
         </section>
         <section><span>Logo</span></section>
